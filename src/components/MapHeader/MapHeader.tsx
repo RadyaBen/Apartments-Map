d947601c707bf278eee8b0f766e820fc5b013130
RadyaBen <lizoguben.radya@gmail.com>
2022-07-07 13:14:00 +0300

    Create a map header

---
+++ src/components/MapHeader/MapHeader.tsx
@@ -0,0 +1,13 @@
+import { AppBar, Toolbar } from '@mui/material';
+
+const MapHeader = () => {
+	return (
+		<AppBar position='static'>
+			<Toolbar>
+				<div>Map Header</div>
+			</Toolbar>
+		</AppBar>
+	);
+};
+
+export { MapHeader }
