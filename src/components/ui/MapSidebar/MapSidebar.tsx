9166e732a7b9f69425fe7306bf8dbcc5138e2058
RadyaBen <lizoguben.radya@gmail.com>
2022-08-01 18:20:23 +0300

    Improve the look of the sidebar using styles

--- src/components/ui/MapSidebar/MapSidebar.tsx
+++ src/components/ui/MapSidebar/MapSidebar.tsx
@@ -3,38 +3,12 @@ import {
 	Card,
 	CardMedia,
 	CardContent,
-	Drawer,
 	Grid,
 	Typography
 } from '@mui/material';
-import { makeStyles } from '@mui/styles';
 
 import { IApartmentItem } from '../../../types/apartments';
-
-const drawerWidth = 280;
-
-const useStyles = makeStyles({
-	drawer: {
-		width: drawerWidth
-	},
-	drawerPaper: {
-		width: drawerWidth
-	},
-	mapCard: {
-		maxWidth: 260,
-		height: 320,
-		margin: 8,
-		borderRadius: 10
-	},
-	image: {
-		minWidth: 250,
-		minHeight: 230
-	},
-	message: {
-		fontSize: [30, '!important'] as unknown as number,
-		margin: 'auto !important' as 'auto'
-	}
-});
+import { useStyles } from './styles';
 
 type ClickedMarkersProps = {
 	clickedMarkers: IApartmentItem[]
@@ -46,42 +20,46 @@ const MapSidebar = ({ clickedMarkers }: ClickedMarkersProps) => {
 
 	return (
 		<>
-			<Drawer
-				className={classes.drawer}
-				variant='permanent'
-				anchor='right'
-				classes={{ paper: classes.drawerPaper }}
-			>
-				{clickedMarkers.length === 0 ? (
-					<Typography className={classes.message}>No apartments yet</Typography>
-				) : (
-					<Grid container spacing={1}>
-						{clickedMarkers && clickedMarkers.map((apartment) => (
-							<Grid item key={apartment.id} xs={12}>
-								<Card elevation={3} className={classes.mapCard} >
-									<CardMedia
-										className={classes.image}
-										component='img'
-										alt={apartment.description}
-										image={apartment.image}
-										title='Apartment Image'
-									/>
-									<CardContent>
-										<Typography variant='body1' component='h2'>
-											<Box fontWeight={600}>{apartment.description}</Box>
-										</Typography>
-										<Typography variant='body2' component='h2'>
-											{apartment.address}
-										</Typography>
-									</CardContent>
-								</Card>
-							</Grid>
-						))}
-					</Grid>
-				)}
-			</Drawer>
+			{clickedMarkers.length === 0 ? (
+				<Typography
+					sx={{
+						fontSize: {
+							xs: 14,
+							sm: 25,
+							md: 20,
+							lg: 30
+						}
+					}}
+				>
+					No apartments yet
+				</Typography>
+			) : (
+				<Grid container spacing={1} style={{ height: 'calc(100vh - 64px)', overflowY: 'auto' }}>
+					{clickedMarkers && clickedMarkers.map((apartment) => (
+						<Grid item xs key={apartment.id} >
+							<Card elevation={3} className={classes.mapCard}>
+								<CardMedia
+									className={classes.image}
+									component='img'
+									alt={apartment.description}
+									image={apartment.image}
+									title='Apartment Image'
+								/>
+								<CardContent>
+									<Typography variant='body1' component='h2'>
+										<Box fontWeight={600}>{apartment.description}</Box>
+									</Typography>
+									<Typography variant='body2' component='h2'>
+										{apartment.address}
+									</Typography>
+								</CardContent>
+							</Card>
+						</Grid>
+					))}
+				</Grid>
+			)}
 		</>
 	);
 };
 
-export { MapSidebar }
+export { MapSidebar };
